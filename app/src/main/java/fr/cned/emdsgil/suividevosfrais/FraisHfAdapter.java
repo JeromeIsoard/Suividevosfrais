5a84f6325d06a272f3a5a2ab9869788f3b9c4a49
JeromeIsoard <isoard.jerome@gmail.com>
2018-04-26 13:23:07 +0200

    Gestion de la suppresion de frais hors forfait

--- app/src/main/java/fr/cned/emdsgil/suividevosfrais/FraisHfAdapter.java
+++ app/src/main/java/fr/cned/emdsgil/suividevosfrais/FraisHfAdapter.java
@@ -1,10 +1,13 @@
 package fr.cned.emdsgil.suividevosfrais;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -12,17 +15,20 @@ import java.util.Locale;
 
 class FraisHfAdapter extends BaseAdapter {
 
-	private final ArrayList<FraisHf> lesFrais ; // liste des frais du mois
-	private final LayoutInflater inflater ;
+	private final ArrayList<FraisHf> lesFrais ; // liste des fraisHf du mois
+	private final LayoutInflater inflater ; // permet de formater la ligne
+	private final Integer key ; // le mois correspondant aux FraisHf en question
 
     /**
 	 * Constructeur de l'adapter pour valoriser les propriétés
      * @param context Accès au contexte de l'application
      * @param lesFrais Liste des frais hors forfait
      */
-	public FraisHfAdapter(Context context, ArrayList<FraisHf> lesFrais) {
+	public FraisHfAdapter(Context context, ArrayList<FraisHf> lesFrais, Integer key) {
 		inflater = LayoutInflater.from(context) ;
 		this.lesFrais = lesFrais ;
+		this.key = key;
+
     }
 	
 	/**
@@ -51,8 +57,10 @@ class FraisHfAdapter extends BaseAdapter {
 
 	/**
 	 * structure contenant les éléments d'une ligne
+	 * ("sous-classe": propriété privée sous forme de classe)
 	 */
 	private class ViewHolder {
+		ImageButton cmdSuppHf ;
 		TextView txtListJour ;
 		TextView txtListMontant ;
 		TextView txtListMotif ;
@@ -62,21 +70,46 @@ class FraisHfAdapter extends BaseAdapter {
 	 * Affichage dans la liste
 	 */
 	@Override
-	public View getView(int index, View convertView, ViewGroup parent) {
+	public View getView(int index, View convertView, final ViewGroup parent) {
 		ViewHolder holder ;
-		if (convertView == null) {
+		if (convertView == null) { // si la ligne n'existe pas, elle est créée
 			holder = new ViewHolder() ;
+			// la ligne est construite avec un formatage (inflater) relié à layout_liste
 			convertView = inflater.inflate(R.layout.layout_liste, parent, false) ;
+			// chaque propriété du holder est reliée à une propriété graphique
+			holder.cmdSuppHf = convertView.findViewById(R.id.cmdSuppHf);
 			holder.txtListJour = convertView.findViewById(R.id.txtListJour);
 			holder.txtListMontant = convertView.findViewById(R.id.txtListMontant);
 			holder.txtListMotif = convertView.findViewById(R.id.txtListMotif);
+			// affecter le holder à la vue, la ligne est construite
 			convertView.setTag(holder) ;
-		}else{
+		}else{ // sinon, la ligne existe et elle est récupérée
 			holder = (ViewHolder)convertView.getTag();
 		}
+		// valorisation du contenu du holder (donc de la ligne)
+		holder.cmdSuppHf.setTag(index); // pour savoir à quelle ligne correspond le bouton
 		holder.txtListJour.setText(String.format(Locale.FRANCE, "%d", lesFrais.get(index).getJour()));
 		holder.txtListMontant.setText(String.format(Locale.FRANCE, "%.2f", lesFrais.get(index).getMontant())) ;
 		holder.txtListMotif.setText(lesFrais.get(index).getMotif()) ;
+		// clic sur la croix pour supprimer le frais
+		holder.cmdSuppHf.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// récupération de l'index de la ligne et du contexte
+				int position = (int)v.getTag();
+				Context contexte = parent.getContext();
+//				Log.d("contexte", "*************************** le contexte : " + contexte);
+//				Log.d("index", "*************************** l'index du fraishF : " + position);
+				
+				// suppression du fraisHf correspondant à l'index de la ligne
+				lesFrais.remove(position);
+				Global.listFraisMois.get(key).supprFraisHf(position);
+				Serializer.serialize(Global.listFraisMois, contexte);
+				// raffraichissement de la vue
+				notifyDataSetChanged();
+			}
+		});
+
 		return convertView ;
 	}
 	
